811d37e21f2cc5ae003a0ab4941d0d36f3a46b7c
Carmen <carmenzrgn13@gmail.com>
2025-11-21 13:05:58 +0100

    Primer commit header

--- ReactVite/src/Mercadona/Web.tsx
+++ ReactVite/src/Mercadona/Web.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './componentes/Header';
-import Footer from './componentes/Footer';
 
 export default function Web() {
     return (
@@ -9,21 +8,8 @@ export default function Web() {
         <Header />
   
         {/* Navbar debajo del header */}
-        <Navbar />
-        
-        <div className={styles.layout}>
-          <Aside />
-  
-          <main className={styles.main}>
-            <Routes>
-              <Route path="/" element={<Inicio />} />
-              <Route path="/noticias" element={<Noticias />} />
-              <Route path="/contacto" element={<Contacto />} />
-            </Routes>
-          </main>
-        </div>
-  
-        <Footer />
+      
+
       </BrowserRouter>
     );
   }
