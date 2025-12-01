5751e9f1d3cb88af9c8aaf9e1ccc169da62e6560
Carmen <carmenzrgn13@gmail.com>
2025-12-01 14:00:48 +0100

    Mejora de codigo

--- ReactVite/src/Mercadona/componentes/Header.tsx
+++ ReactVite/src/Mercadona/componentes/Header.tsx
@@ -5,19 +5,13 @@ import { useState } from 'react';
 // Componente simulado para el selector de idioma
 const LanguageSelector = () => (
     <div className={estilos.languageSelector}>
-        Español <span style={{ marginLeft: '5px' }}>&#9660;</span> {/* Flecha hacia abajo */}
+        Español <span style={{ marginLeft: '5px' }}>&#9660;</span> 
     </div>
 );
 
 export default function Header() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
- // Usamos la URL del logo de la imagen
     const logoSrc = "/mercadona.png"; 
-    
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
 
     return (
         <div className={estilos.headerWrapper}>
@@ -26,9 +20,7 @@ export default function Header() {
             <nav className={estilos.nav}> 
             {/* Logo */}
                 <img  src={logoSrc}  alt="Logo de Mercadona" style={{ height: '35px' }} className={estilos.logo} />
-                {/* Contenedor de los enlaces */}
-                {/* Contenedor de los enlaces. La clase 'menuOpen' se añade si el menú está abierto */}
-                <div className={`${estilos.linksContainer} ${isMenuOpen ? estilos.menuOpen : ''}`}>
+                <div className={estilos.linksContainer}>
                     <Link to="/Conocenos">Conócenos</Link>
                     <Link to="/paginas/Supermercados">Supermercados</Link>
                     <Link to="/paginas/TrabajaConNosotros">Trabaja con nosotros</Link>
