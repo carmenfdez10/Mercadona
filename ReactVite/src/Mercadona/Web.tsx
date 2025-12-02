be08e04e0554f236b25629cc71f1a6624a4e241b
Carmen <carmenzrgn13@gmail.com>
2025-12-02 20:55:26 +0100

    Funcionalidad de los enlaces

--- ReactVite/src/Mercadona/Web.tsx
+++ ReactVite/src/Mercadona/Web.tsx
@@ -1,4 +1,5 @@
 import { Outlet, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './componentes/Header';
 import Conocenos from './paginas/Conocenos';
 import Supermercado from './paginas/Supermercados';
@@ -12,6 +13,15 @@ export default function Web() {
         <Header />
         {/* Outlet renderiza lo que diga la Ruta hija (el Body) */}
         <Outlet />
+        <main>
+          <Routes>
+            <Route path="/ReactVite/src/Mercadona/paginas/Conocenos.tsx" element={<Conocenos />} />
+            <Route path="/Conocenos" element={<Conocenos />} />
+            <Route path="/paginas/Supermercados" element={<Supermercado />} />
+            <Route path="/paginas/TrabajaConNosotros" element={<TrabajaConNostros />} />
+            <Route path="/paginas/AtencionAlCliente" element={<AtencionAlCliente />} />
+          </Routes>
+        </main>
         <Footer />
 
       </BrowserRouter>
